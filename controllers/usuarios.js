2499385962e4dab238a2580a6fc9deb86a7c6594
Juan Cruz Marquez <marquezjuanchy@hotmail.com>
2021-08-22 00:42:11 -0300

    Backend Basico

---
+++ controllers/usuarios.js
@@ -0,0 +1,52 @@
+
+const { response } = require('express');
+
+const usuariosGet = (req, res= response)   => {
+
+    const {q,nombre,apikey} = req.query;
+
+
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
+    })
+}
+
+const usuariosPost = (req, res= response) => {
+    
+    const body = req.body;
+    
+    
+    res.json({
+         msg: 'post API - controlador',
+         body
+    })
+}  
+
+const usuariosPut = (req, res= response) => {
+    
+    const id = req.params.id
+   
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+}    
+
+const usuariosDelete = (req, res= response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+}    
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
